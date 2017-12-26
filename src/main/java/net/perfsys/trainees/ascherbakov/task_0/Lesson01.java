ea309c7d493c5585c0f31c392f884e0386697878
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2017-12-26 22:44:21 +0200

    Update and rename lesson0_1.java to Lesson01.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson01.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson01.java
@@ -1,9 +1,9 @@
 package net.perfsys.trainees.ascherbakov.task_0;
 
 public class Lesson01 {
-     public static void main(String args[]){
-        int a = 36;
-        int b = 7;
-        System.out.println(" Результат " + a/b + " в остатке " + a % b);
+    public static void main(String args[]){
+        int a = 6;
+        int b = 9;
+        System.out.println(rez = a % b);
     }
 }
