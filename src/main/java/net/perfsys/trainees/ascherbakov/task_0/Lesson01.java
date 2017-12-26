16211883661308943d38d08461da0c495ef586b8
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2017-12-26 22:44:42 +0200

    Update Lesson01.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson01.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson01.java
@@ -4,6 +4,6 @@ public class Lesson01 {
     public static void main(String args[]){
         int a = 6;
         int b = 9;
-        System.out.println(rez = a % b);
+        System.out.println(a % b);
     }
 }
