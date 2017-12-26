a7dab872166d1b225b4b0bf406bce65267afca90
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2017-12-26 21:38:06 +0200

    Update and rename Main.java to lesson0_1.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Main.java
+++
@@ -1,9 +0,0 @@
-package net.perfsys.trainees.ascherbakov.task_0;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        System.out.println("test");
-    }
-}
---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lesson0_1.java
@@ -0,0 +1,10 @@
+package net.perfsys.trainees.ascherbakov.task_0;
+
+public class First {
+    public static void main(String args[]){
+        int a = 6;
+        int b = 9;
+        int rez;
+        System.out.println(rez = a % b);
+    }
+}
