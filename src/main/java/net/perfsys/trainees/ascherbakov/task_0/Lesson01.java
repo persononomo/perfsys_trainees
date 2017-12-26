46bd88de8afafc159d6ed981da073e500a0a433c
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2017-12-26 22:44:21 +0200

    Update and rename lesson0_1.java to Lesson01.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson01.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson01.java
@@ -1,10 +1,9 @@
 package net.perfsys.trainees.ascherbakov.task_0;
 
-public class First {
+public class Lesson01 {
     public static void main(String args[]){
         int a = 6;
         int b = 9;
-        int rez;
         System.out.println(rez = a % b);
     }
 }
