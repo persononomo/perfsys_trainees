9462d6dfa1d90adf2804db4d6752544295220c7a
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2017-12-27 11:53:37 +0200

    Create Lesson02.java

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson02.java
@@ -0,0 +1,12 @@
+package net.perfsys.trainees.ascherbakov.task_0;
+
+public class Lesson02 {
+    public static void main(String args []){
+
+        int a = 28;
+        int b = a%10;
+        int c = a/10;
+        int rez = b+c;
+        System.out.println(" Результат сложения цифр числа а " + rez);
+    }
+}
