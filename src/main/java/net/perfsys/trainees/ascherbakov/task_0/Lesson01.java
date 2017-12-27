d0039f2b32e1f89ad03983bf812d8e3586fa015a
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2017-12-27 08:54:11 +0200

    Update Lesson01.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson01.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson01.java
@@ -4,6 +4,6 @@ public class Lesson01 {
      public static void main(String args[]){
         int a = 36;
         int b = 7;
-        System.out.println(" Результат " + (a/b) + " в остатке " + (a % b));
+        System.out.println(" Результат " + a/b + " в остатке " + a % b);
     }
 }
