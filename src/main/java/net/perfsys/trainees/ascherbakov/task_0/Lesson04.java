f5cad88c74fe61242b11630ba03c33395d56437f
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2017-12-28 15:04:29 +0200

    Create Lesson04.java

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson04.java
@@ -0,0 +1,19 @@
+package net.perfsys.trainees.ascherbakov.task_0;
+
+import java.util.Scanner;
+
+public class Lesson04 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int sum = 0;
+        sum(n, sum);
+    }
+
+    public static void sum(int n, int sum) {
+        if (n != 0) {
+            sum += n % 10;
+            sum(n / 10, sum);
+        } else System.out.println(sum);
+    }
+}
