1b75e9d2c11e32b80edebe29a630d5145e2fdab3
andrei <kupcovandrei12@gmail.com>
2017-12-28 15:54:10 +0200

    task_3 new

--- src/main/java/net/perfsys/trainees/akupcov/task_3/NNumbers.java
+++ src/main/java/net/perfsys/trainees/akupcov/task_3/NNumbers.java
@@ -4,20 +4,25 @@ package net.perfsys.trainees.akupcov.task_3;
 
 public class NNumbers {
     public static void main(String[] args) {
-        int nNumber = SumNNumbers(5679);
+        int nNumber = SumNNumbers(66768);
         System.out.println(nNumber);
 
     }
 
     public static int SumNNumbers(int a) {
-        int n = 0;
-        String Number = String.valueOf(a);
-        char[] strToArray = Number.toCharArray();
-        for(int i = 0; i < strToArray.length; i++){
-            int b = Character.getNumericValue(strToArray[i]);
-            n+=b;
+        int n = a;
+//        String Number = String.valueOf(a);
+//        char[] strToArray = Number.toCharArray();
+//        for(int i = 0; i < strToArray.length; i++){
+//            int b = Character.getNumericValue(strToArray[i]);
+//            n+=b;
+//        }
+        if( n < 10) {
+            return n;
+        }else {
+            return n % 10 + SumNNumbers(n / 10);
         }
 
-        return n;
+//        return n;
     }
 }
