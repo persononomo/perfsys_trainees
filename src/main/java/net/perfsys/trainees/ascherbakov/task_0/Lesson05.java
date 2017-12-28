48fad001eb56fc63c96c0be93f4991cde1db5861
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2017-12-28 14:40:35 +0200

    Create Lesson05.java

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Lesson05.java
@@ -0,0 +1,10 @@
+package net.perfsys.trainees.ascherbakov.task_0;
+
+public static void main(String args []){
+        int a = 0;
+        int b = 15;
+
+        int random_number = a + (int)(Math.random()*b);
+        System.out.println(random_number);
+    }
+}
