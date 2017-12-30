7ce0cc937f514883c22e04e920cf96dee5f2f3a2
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2017-12-30 14:29:57 +0200

    Update calc.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/calc.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/calc.java
@@ -1,3 +1,5 @@
+package net.perfsys.trainees.ascherbakov.task_0;
+
 import java.util.Scanner;
 
 public class calc {
@@ -6,29 +8,48 @@ public class calc {
     public static void main(String[] args) {
 
         Scanner scn = new Scanner(System.in);
-        int a = scn.nextInt();
-        int b = scn.nextInt();
-        char ch1 = '+';
-        char ch2 = '-';
-        char ch3 = '/';
-        char ch4 = '*';
+        int a, b, result;
+        char operation = 'o';
 
 
-        switch (scn.nextInt()) {
+        switch (operation) {
             case '+':
-                System.out.println(a + b);
+                System.out.println("Введи первое число");
+                a = scn.nextInt();
+                System.out.println("Введи второе число");
+                b = scn.nextInt();
+
+                result = a + b;
+                System.out.println("Сумма равна " + result);
                 break;
             case '-':
-                System.out.println(a - b);
+                System.out.println("Введи первое число");
+                a = scn.nextInt();
+                System.out.println("Введи второе число");
+                b = scn.nextInt();
+
+                result = a - b;
+                System.out.println("Разница " + result);
                 break;
             case '/':
-                System.out.println(a / b);
+                System.out.println("Введи первое число");
+                a = scn.nextInt();
+                System.out.println("Введи второе число");
+                b = scn.nextInt();
+
+                result = a / b;
+                System.out.println("Результат деления " + result);
                 break;
             case '*':
-                System.out.println(a * b);
+                System.out.println("Введи первое число");
+                a = scn.nextInt();
+                System.out.println("Введи второе число");
+                b = scn.nextInt();
+
+                result = a * b;
+                System.out.println("Результат умножения " + result);
                 break;
         }
-
-
     }
+
 }
