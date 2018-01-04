14ccda5b4980cddcfff08df80da5acc7d1631d45
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-01-04 13:32:17 +0200

    Update and rename calc.java to Calc.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Calc.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Calc.java
@@ -3,32 +3,52 @@ package net.perfsys.trainees.ascherbakov.task_0;
 import java.util.Scanner;
 
 public class Calc {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Введи операцию");
-        String operation = scn.next();
 
-        double a, b;
-        System.out.println("Введи первое число");
-        a = scn.nextInt();
-        System.out.println("Введи второе число");
-        b = scn.nextInt();
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int a, b, result;
+        char operation = 'o';
+
 
         switch (operation) {
-            case "+":
-                System.out.println("Сумма равна " + (a + b));
+            case '+':
+                System.out.println("Введи первое число");
+                a = scn.nextInt();
+                System.out.println("Введи второе число");
+                b = scn.nextInt();
+
+                result = a + b;
+                System.out.println("Сумма равна " + result);
                 break;
-            case "-":
-                System.out.println("Разница " + (a - b));
+            case '-':
+                System.out.println("Введи первое число");
+                a = scn.nextInt();
+                System.out.println("Введи второе число");
+                b = scn.nextInt();
+
+                result = a - b;
+                System.out.println("Разница " + result);
                 break;
-            case "/":
-                System.out.println("Результат деления " + (a / b));
+            case '/':
+                System.out.println("Введи первое число");
+                a = scn.nextInt();
+                System.out.println("Введи второе число");
+                b = scn.nextInt();
+
+                result = a / b;
+                System.out.println("Результат деления " + result);
                 break;
-            case "*":
-                System.out.println("Результат умножения " + (a * b));
+            case '*':
+                System.out.println("Введи первое число");
+                a = scn.nextInt();
+                System.out.println("Введи второе число");
+                b = scn.nextInt();
+
+                result = a * b;
+                System.out.println("Результат умножения " + result);
                 break;
-            default:
-                System.out.println("Error");
         }
     }
 
