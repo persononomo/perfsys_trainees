249bfbad319fa07734c75868759fd8b1b43687c0
andrei <kupcovandrei12@gmail.com>
2018-01-04 20:47:08 +0200

    task_6 new

--- src/main/java/net/perfsys/trainees/akupcov/task_6/Snake.java
+++ src/main/java/net/perfsys/trainees/akupcov/task_6/Snake.java
@@ -2,20 +2,64 @@ package net.perfsys.trainees.akupcov.task_6;
 
 public class Snake {
     public static void main(String[] args) {
+        int[][] numTwo = new int[9][9];
+        numTwo = snakeNum1(numTwo, 1, 0, 0, 0); // для коректной работы вводить 1,0,0,0
 
-        int num[][] = {
-                {1, 2, 3, 4, 5},
-                {16, 17, 18, 19, 6},
-                {15, 24, 25, 20, 7},
-                {14, 23, 22, 21, 8},
-                {13, 12, 11, 10, 9},
-        };
-        for (int i = 0; i < num.length; i++) {
-            for (int j = 0; j < num.length; j++) {
-                System.out.print(num[i][j] + "\t");
+        for (int i = 0; i < numTwo.length; i++) {
+            for (int j = 0; j < numTwo.length; j++) {
+                System.out.print(numTwo[i][j] + "\t");
             }
             System.out.println();
         }
+    }
 
+    public static int[][] snakeNum1(int[][] array, int k1, int a1, int t1, int m1) {
+        int[][] num = array;
+        int k = k1;
+        int i, j;
+        int a = a1;
+        int t = t1;
+        int m = m1;
+        int p = 0;
+        if(num.length % 2 == 0){
+            p = num.length / 2;}
+        else{ p = (num.length/2) + 1;}
+
+        for (i = t; i < num.length - t; i++) {
+            for (j = t; j < num.length - t; j++) {
+                num[i][j] = a;
+                a++;
+
+                if (j == num.length - k) {
+                    for (i = k; i < num.length - t; i++) {
+                        num[i][j] = a;
+                        a++;
+                    }
+                    k++;
+                    m++;
+                }
+            }
+        }
+        for (i = num.length - k; i >= t; i--) {
+            for (j = num.length - k; j >= t; j--) {
+                i = num.length - m;
+                num[i][j] = a;
+                a++;
+                if (j == t) {
+                    j = t;
+                    for (i = num.length - k; i > t; i--) {
+                        num[i][j] = a;
+                        a++;
+                    }
+
+                }
+            }
+        }
+        t++;
+        if (k <= p) {
+            return snakeNum1(num, k, a, t, m);
+        } else {
+            return num;
+        }
     }
 }
