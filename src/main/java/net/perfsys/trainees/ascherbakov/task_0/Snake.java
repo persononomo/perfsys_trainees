1997a751aa3762e8f4258cbfd0d7ec899e6d5c05
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-01-11 16:43:08 +0200

    Update Snake.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
@@ -1,26 +1,23 @@
 package net.perfsys.trainees.ascherbakov.task_0;
 import java.util.Random;
-import java.util.Arrays;
+
 
 public class Snake {
     public static void main(String[] args) {
-        //обьявление переменной рандома;
-        Random r = new Random();
-        //длина массива значений случайных чисел от 0 до 99, чтобы заполнить матрицу 10х10;
-        int lengthArray = 99;
-        //обьявление массива 10х10;
+//1) Создать массив из 100 элементов 10х10;
         int[][] newArray = new int[10][10];
-        //инициализация 10 строк массива;
         for (int i = 0; i < newArray.length; i++) {
-            //инициализация 10 столбцов массива;       
             for (int j = 0; j < newArray.length; j++) {
-                //заполнения массива 10х10 случайными числами;   
+//2) Заполнить массив случайными натуральными числами от 0 до 99;
+                Random r = new Random();
+                int lengthArray = 99;
                 newArray[i][j] = r.nextInt(lengthArray);
-                //вывод на экран массива 10х10 из случайных чисел;
-                System.out.print(newArray[i][j] + "\t");        
+//3) Отсортировать массив от минимального к максимальному значению;
+
+
+                System.out.print(newArray[i][j] + "\t");
             }
             System.out.println();
         }
     }
 }
-
