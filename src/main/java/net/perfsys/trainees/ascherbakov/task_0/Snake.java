a49d3e3627398c7531dc190716f10d6c1b6ab920
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-01-12 14:59:13 +0200

    Update Snake.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
@@ -12,8 +12,18 @@ public class Snake {
             array[i] = r.nextInt(maxValueOfArray);
         }
 //3) Отсортировать массив;
-        Arrays.sort(array);
-        for (int i = 0; i < array.length; i++) {
+        int n = array.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++){
+            for(int j=1; j <(n-i); j++){
+                if(array[j-1]>array[j]){
+                    temp = array[j-1];
+                    array[j-1]=array[j];
+                    array[j] = temp;
+                }
+            }
+        //Arrays.sort(array);
+        //for (int i = 0; i < array.length; i++) {
 //4) Создать матрицу 10 на 10;
             int wImg = 10, hImg = 10, count = 0;
             int[][] matrix = new int[wImg][hImg];
