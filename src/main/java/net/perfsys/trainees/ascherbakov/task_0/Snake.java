d8c43dc9d07c7d85a299fdbb88d53d67ab4a16e3
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-01-12 11:14:17 +0200

    Update Snake.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
@@ -4,20 +4,14 @@ import java.util.Random;
 
 public class Snake {
     public static void main(String[] args) {
-//1) Создать массив из 100 элементов 10х10;
-        int[][] newArray = new int[10][10];
-        for (int i = 0; i < newArray.length; i++) {
-            for (int j = 0; j < newArray.length; j++) {
+//1) Создать массив из 100 элементов;
+        int[] Array = new int[99];
 //2) Заполнить массив случайными натуральными числами от 0 до 99;
-                Random r = new Random();
-                int lengthArray = 99;
-                newArray[i][j] = r.nextInt(lengthArray);
-//3) Отсортировать массив от минимального к максимальному значению;
-
-
-                System.out.print(newArray[i][j] + "\t");
-            }
-            System.out.println();
+        Random r = new Random();
+        int lengthArray = 99;
+        for(int i =0;i<Array.length;i++){
+            Array [i] = r.nextInt(lengthArray);
         }
-    }
-}
+//3) Отсортировать массив;
+        Arrays.sort(Array);
+        System.out.print(Array +"\t");
