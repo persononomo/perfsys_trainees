d2b2cde016670d42d6a58585804f8a38f1a53293
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-01-12 14:17:14 +0200

    Update Snake.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
@@ -1,17 +1,32 @@
-package net.perfsys.trainees.ascherbakov.task_0;
-import java.util.Random;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Snake {
     public static void main(String[] args) {
 //1) Создать массив из 100 элементов;
-        int[] Array = new int[100];
+        int[] array = new int[100];
 //2) Заполнить массив случайными натуральными числами от 0 до 99;
         Random r = new Random();
-        int lengthArray = 99;
-        for(int i =0;i<Array.length;i++){
-            Array [i] = r.nextInt(lengthArray);
+        int maxValueOfArray = 99;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt(maxValueOfArray);
         }
 //3) Отсортировать массив;
-        Arrays.sort(Array);
-        System.out.print(Array +"\t");
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+//4) Создать матрицу 10 на 10;
+            int wImg = 10, hImg = 10, count = 0;
+            int[][] matrix = new int[wImg][hImg];
+            for (int row = 0; row < matrix.length; row++) {
+                for (int column = 0; column < matrix[row].length; column++) {
+//5) Перенести одномерный отсортированный массив в двухмерный массив;
+                    matrix[row][column] = array[count++];
+                    System.out.print(matrix[row][column] + "\t");
+                }
+                System.out.println();
+
+
+            }
+        }
+    }
+}
