b67680c2bf30470c41f99aa26a3db81ec40b533e
ascherbakov <crusader34@gmail.com>
2018-01-14 17:36:34 +0200

    new

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Snake.java
@@ -1,4 +1,7 @@
-import java.util.Arrays;
+package net.perfsys.trainees.ascherbakov.task_0;
+
+//import java.util.Arrays;
+
 import java.util.Random;
 
 public class Snake {
@@ -12,31 +15,35 @@ public class Snake {
             array[i] = r.nextInt(maxValueOfArray);
         }
 //3) Отсортировать массив;
-        int n = array.length;
-        int temp = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < (n - i); j++) {
-                if (array[j - 1] > array[j]) {
-                    temp = array[j - 1];
-                    array[j - 1] = array[j];
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
                     array[j] = temp;
                 }
             }
         }
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i] + "\t");
+//        }
+
         //Arrays.sort(array);
         //for (int i = 0; i < array.length; i++) {
 //4) Создать матрицу 10 на 10;
-            int wImg = 10, hImg = 10, count = 0;
-            int[][] matrix = new int[wImg][hImg];
-            for (int row = 0; row < matrix.length; row++) {
-                for (int column = 0; column < matrix[row].length; column++) {
+        int wImg = 10, hImg = 10, count = 0;
+        int[][] matrix = new int[wImg][hImg];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
 //5) Перенести одномерный отсортированный массив в двухмерный массив;
-                    matrix[row][column] = array[count++];
-                    System.out.print(matrix[row][column] + "\t");
-                }
-                System.out.println();
-
-
+                matrix[row][column] = array[count++];
+                System.out.print(matrix[row][column] + "\t");
             }
+            System.out.println();
+
+
         }
     }
+}
+
