50b75d470b8d40336a8d1f05d9bda7e014bcc6cc
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-01-25 10:11:54 +0200

    Create FindFac.java

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/FindFac.java
@@ -0,0 +1,13 @@
+package net.perfsys.trainees.ascherbakov.task_0;
+
+public class FindFac {
+        public static void main(String args[]){
+ for(int i=2; i <= 100; i++) {
+            System.out.print("Дeлитeли " + i + ": ");
+            for(int j = 2; j < i; j++)
+                if((i%j) == 0) System.out.print(j + " ");
+            System.out.println();
+
+        }
+    }
+}
