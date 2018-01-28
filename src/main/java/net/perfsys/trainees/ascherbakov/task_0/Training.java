1828a9f39b973750011b4385e5fa0945c927223b
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-01-28 11:28:54 +0200

    Update Training.java

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/Training.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/Training.java
@@ -1,21 +1,27 @@
 package net.perfsys.trainees.ascherbakov.task_0;
 
-// Создать статический метод,который будет иметь два целочисленных параметра a и b,и в качестве своего значения
-// возвращать случайное целое число из отрезка[a;b].C помощью данного метода заполнить массив из 20целых
+// 1) Создать статический метод,который будет иметь два целочисленных параметра a и b,и в качестве своего значения
+// возвращать случайное целое число из отрезка[a;b].C помощью данного метода заполнить массив из 20 целых
 // чисел и вывести его на экран.
+// 2) Создать метод, который будет выводить указанный массив на экран в строку. С помощью созданного метода и метода
+// из предыдущей задачи заполнить 5 массивов из 10 элементов каждый случайными числами и вывести все 5 массивов на
+// экран, каждый на отдельной строке.
+
 
 public class Training {
     public static void main(String[] args) {
-        array();
+        arrayFiveTen();
+        arrayFiveTen();
+        arrayFiveTen();
+        arrayFiveTen();
+        arrayFiveTen();
+
     }
 
     private static int numbers() {
         int a = 0;
         int b = 78;
-        int c;
-        c = (int) (Math.random() * (b - a) + a);
-        return c;
-
+        return (int) (Math.random() * (b - a) + a);
     }
 
     private static void array() {
@@ -25,6 +31,16 @@ public class Training {
             System.out.print(newArray[i] + "\t");
         }
         System.out.println();
+    }
+
+    private static void arrayFiveTen() {
+        int[] newArrayFiveTen = new int[10];
+        for (int i = 0; i < newArrayFiveTen.length; i++) {
+            newArrayFiveTen[i] = numbers();
+            System.out.print(newArrayFiveTen[i] + "\t");
+        }
+        System.out.println();
+
     }
 
 }
