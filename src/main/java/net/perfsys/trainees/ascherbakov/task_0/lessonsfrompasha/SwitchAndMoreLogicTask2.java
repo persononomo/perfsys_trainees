9fa2a90e43d8037b666482726146a8900c7aca28
ascherbakov <crusader34@gmail.com>
2018-01-30 23:03:22 +0200

    fix

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonsfrompasha/SwitchAndMoreLogicTask2.java
@@ -0,0 +1,18 @@
+//Создать программу, выводящую на экран случайно сгенерированное трёхзначное натуральное число и его наибольшую цифру.
+
+package net.perfsys.trainees.ascherbakov.task_0.lessonsfrompasha;
+
+public class SwitchAndMoreLogicTask2 {
+    public static void main(String[] args) {
+        int r = random();
+        int a = r/100;
+        int b = (r%100)/10;
+        int c = r%10;
+        System.out.println();
+    }
+    private static int random() {
+        int a = 100;
+        int b = 999;
+        return (int) (Math.random() * (b - a) + a);
+    }
+}
