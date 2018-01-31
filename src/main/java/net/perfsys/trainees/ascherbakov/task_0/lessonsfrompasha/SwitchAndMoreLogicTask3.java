2c4bf9d062f172c3d431c021466b58817c1ae933
ascherbakov <crusader34@gmail.com>
2018-01-31 20:08:26 +0200

    fix

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonsfrompasha/SwitchAndMoreLogicTask3.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonsfrompasha/SwitchAndMoreLogicTask3.java
@@ -1,9 +1,8 @@
-package net.perfsys.trainees.ascherbakov.task_0.lessonsfrompasha;
-
 
 //В три переменные a, b и c явно записаны программистом три целых попарно неравных между собой числа.
 //Создать программу, которая переставит числа в переменных таким образом,
 //чтобы при выводе на экран последовательность a, b и c оказалась строго возрастающей.
+package net.perfsys.trainees.ascherbakov.task_0.lessonsfrompasha;
 
 public class SwitchAndMoreLogicTask3 {
     public static void main(String[] args) {
