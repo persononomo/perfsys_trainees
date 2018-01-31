ef7927ebf9461277f8c492ae54431bfd2e68cf46
ascherbakov <crusader34@gmail.com>
2018-01-31 19:34:33 +0200

    fix

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonsfrompasha/SwitchAndMoreLogicTask3.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonsfrompasha/SwitchAndMoreLogicTask3.java
@@ -1,5 +1,4 @@
 package net.perfsys.trainees.ascherbakov.task_0.lessonsfrompasha;
-import java.util.Arrays;
 
 
 //В три переменные a, b и c явно записаны программистом три целых попарно неравных между собой числа.
@@ -8,23 +7,34 @@ import java.util.Arrays;
 
 public class SwitchAndMoreLogicTask3 {
     public static void main(String[] args) {
-        int a = 56; int b = 54; int c = 1;
-        int [] array = {a,b,c};
-        Arrays.sort(array);
-        for (int i = 0; i <array.length ; i++) {
-            System.out.print(array[i] + "\t");
+        int a = 276;
+        int b = 983;
+        int c = 129;
+        int temp;
+//        int [] array = {a,b,c};
+//        Arrays.sort(array);
+//        for (int i = 0; i <array.length ; i++) {
+//            System.out.print(array[i] + "\t");
+//        }
+//        System.out.println();
+        System.out.println("Числа в переменных a, b, c : " + a + "\t" + "\t" + b + "\t" + "\t" + c);
+        if (a > b) {
+            temp = a;
+            a = b;
+            b = temp;
         }
-        System.out.println();
-//        System.out.println("Числа в переменных a, b, c : " + a+ "\t"+"\t"+ b +"\t"+ "\t"+c);
-//        if(a>b&&a>c){
-//            System.out.println("Типо сортировка a, b, c : " + b+ "\t"+"\t"+ c +"\t"+ "\t"+a);
-//        }
-//        if(b>c&&b>a){
-//            System.out.println("Типо сортировка a, b, c : " + c+ "\t"+"\t"+ a +"\t"+ "\t"+b);
-//        }
-//        if(c<=a&&c<=b) {
-//            a = c;
-//        }
-
+        if (b > c) {
+            temp = b;
+            b = c;
+            c = temp;
+        }
+        if (a > b) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        System.out.println("Типо сортировка a, b, c : " + a + "\t" + "\t" + b + "\t" + "\t" + c);
     }
+
 }
+
