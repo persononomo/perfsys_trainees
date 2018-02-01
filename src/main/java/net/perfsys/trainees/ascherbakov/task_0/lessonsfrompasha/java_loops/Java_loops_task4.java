fbee9b887eab885787fd668d61b6d24e03ebfc1f
ascherbakov <crusader34@gmail.com>
2018-02-01 10:47:48 +0200

    fix

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonsfrompasha/java_loops/Java_loops_task4.java
@@ -0,0 +1,11 @@
+
+
+package net.perfsys.trainees.ascherbakov.task_0.lessonsfrompasha.java_loops;
+
+public class Java_loops_task4 {
+    public static void main(String[] args) {
+        for (int a = 1,b = 1; a <=20 ; a++,b*=2) {
+            System.out.println(a+"." + " " + b + " ");
+        }
+    }
+}
