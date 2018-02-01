36a3e58771b80ae826d92d12766dc86705b31966
ascherbakov <crusader34@gmail.com>
2018-02-01 20:07:23 +0200

    fix

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonsfrompasha/java_switch_and_more_logic/SwitchAndMoreLogicTask4.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonsfrompasha/java_switch_and_more_logic/SwitchAndMoreLogicTask4.java
@@ -6,18 +6,18 @@ package net.perfsys.trainees.ascherbakov.task_0.lessonsfrompasha.java_switch_and
 
 public class SwitchAndMoreLogicTask4 {
     public static void main(String[] args) {
-       int n = random();
-       int sec = 3600;
-       int hour = n/3600;
+        int n = random();
+        int sec = 3600;
+        int hour = n / 3600;
         System.out.println(n);
-       if(n<3600){
-           System.out.println("Less then hour remaining");
-       }
-       else {
-            System.out.println(  hour +" "+ "hour"+"  " +"  "+ "remaining");
+        if (n < 3600) {
+            System.out.println("Less then hour remaining");
+        } else {
+            System.out.println(hour + " " + "hour" + "  " + "  " + "remaining");
         }
 
     }
+
     public static int random() {
         int a = 0;
         int b = 28800;
