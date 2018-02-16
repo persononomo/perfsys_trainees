12008c75e4034044f714566391545f7a6bdc547e
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-02-16 14:22:40 +0200

    Create LinearProgrammingTask2.java

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonfromelena/LinearProgrammingTask2.java
@@ -0,0 +1,30 @@
+//В банкомате имеются купюры номиналом 200 грн., 50 грн. и 10 грн.
+//Как наименьшим количеством купюр можно выдать сумму в S гривен? Указать количество купюр каждого номинала.
+//Предполагается, что в банкомате имеется достаточно большое количество купюр каждого номинала. 
+
+package net.perfsys.trainees.ascherbakov.task_0.lessonfromelena;
+
+public class LinearProgrammingTask2 {
+   public static void main(String[] args) {
+        int s = 1565;
+        int a = 200;
+        int b = 50;
+        int c = 10;
+        int d = 0;
+        int b200 = 0;
+        int b50 = 0;
+        int b10 = 0;
+
+        d = s;
+        b200 = d / a;
+        d = s - (b200 * a);
+        b50 = d / b;
+        d = s - b200 * a - b50 * b;
+        b10 = d / c;
+        d = s - b200 * a - b50 * b - b10 * c;
+
+
+        System.out.println(b200 + " " + b50 + " " +b10 + " "+ d);
+    }
+
+}
