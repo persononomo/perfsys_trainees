9395ed76d3f83488acae71a82d7a998a237eca85
ascherbakov <crusader34@gmail.com>
2018-02-20 20:35:52 +0200

    fix

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonfromelena/LinearProgrammingTask11.java
@@ -0,0 +1,17 @@
+//Пользователь вводит четырехзначное число. В нем меняют местами тысячи с сотнями и десятки с единицами.
+//Найти сумму исходного числа и нового полученного числа. (Например, введено 1234, получено 2143, сумма 1234+2143=3377).
+
+package net.perfsys.trainees.ascherbakov.task_0.lessonfromelena;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LinearProgrammingTask11 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите трехзначное число:");
+        String str = scn.nextLine();
+        int[] numArr = Arrays.stream(str.split("")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(numArr);
+    }
+}
