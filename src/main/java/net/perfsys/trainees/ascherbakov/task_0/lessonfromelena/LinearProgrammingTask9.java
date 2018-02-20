26b4b8594a664d7d445acbdaf708b63e234f6fe1
pkozub <pkozub@perfsys.net>
2018-02-20 21:33:10 +0200

    .

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonfromelena/LinearProgrammingTask9.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonfromelena/LinearProgrammingTask9.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.lang.Math.abs;
+
 public class LinearProgrammingTask9 {
     public static void main2(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -24,13 +26,14 @@ public class LinearProgrammingTask9 {
         System.out.println("Максимальная цифра в числе " + c + "," + " Минимальная цифра в числе " + c2);
 
     }
+
     public static void main3(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Введите трехзначное число:");
         String str = scn.nextLine();
         int[] numArr = Arrays.stream(str.split("")).mapToInt(Integer::parseInt).sorted().toArray();
 
-        System.out.println("Максимальная цифра в числе " + numArr[numArr.length -1] + "," + " Минимальная цифра в числе " + numArr[0]);
+        System.out.println("Максимальная цифра в числе " + numArr[numArr.length - 1] + "," + " Минимальная цифра в числе " + numArr[0]);
 
     }
 
@@ -39,10 +42,25 @@ public class LinearProgrammingTask9 {
         System.out.println("Введите трехзначное число:");
         String str = scn.nextLine();
 
-        OptionalInt optionalMax = Arrays.stream(str.split("")).mapToInt(Integer::parseInt).max();
-        OptionalInt optionalMin =  Arrays.stream(str.split("")).mapToInt(Integer::parseInt).min();
-        int max = optionalMax.orElse(0);
-        int min = optionalMin.orElse(0);
+        int[] numArr = Arrays.stream(str.split("")).mapToInt(Integer::parseInt).toArray();
+        int a = numArr[0];
+        int b = numArr[1];
+        int c = numArr[2];
+
+        int max = (abs(2 * c - abs(b - a) - b - a) + 2 * c + abs(b - a) + b + a) / 4;
+
+        int indexMax = str.indexOf(String.valueOf(max));
+        String str_mid = str.substring(0, indexMax) + str.substring(indexMax + 1);
+        int[] numArr_short = Arrays.stream(str_mid.split("")).mapToInt(Integer::parseInt).toArray();
+
+        int x = numArr_short[0];
+        int y = numArr_short[1];
+        int mid = (x + y + abs(x - y)) / 2;
+
+        int indexMid = str.indexOf(String.valueOf(mid));
+        String str_min = str.substring(0, indexMid) + str.substring(indexMid + 1);
+
+        int min = Arrays.stream(str_min.split("")).mapToInt(Integer::parseInt).toArray()[0];
 
         System.out.println("Максимальная цифра в числе " + max + "," + " Минимальная цифра в числе " + min);
 
