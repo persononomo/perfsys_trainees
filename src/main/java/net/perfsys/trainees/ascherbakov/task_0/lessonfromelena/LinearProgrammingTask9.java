1a7cc71d2c1bafc58077f82e9feba59d03c2e778
pkozub <pkozub@perfsys.net>
2018-02-20 20:48:51 +0200

    .

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonfromelena/LinearProgrammingTask9.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonfromelena/LinearProgrammingTask9.java
@@ -3,7 +3,11 @@
 package net.perfsys.trainees.ascherbakov.task_0.lessonfromelena;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LinearProgrammingTask9 {
     public static void main2(String[] args) {
@@ -20,13 +24,27 @@ public class LinearProgrammingTask9 {
         System.out.println("Максимальная цифра в числе " + c + "," + " Минимальная цифра в числе " + c2);
 
     }
-    public static void main(String[] args) {
+    public static void main3(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Введите трехзначное число:");
         String str = scn.nextLine();
         int[] numArr = Arrays.stream(str.split("")).mapToInt(Integer::parseInt).sorted().toArray();
 
         System.out.println("Максимальная цифра в числе " + numArr[numArr.length -1] + "," + " Минимальная цифра в числе " + numArr[0]);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите трехзначное число:");
+        String str = scn.nextLine();
+
+        OptionalInt optionalMax = Arrays.stream(str.split("")).mapToInt(Integer::parseInt).max();
+        OptionalInt optionalMin =  Arrays.stream(str.split("")).mapToInt(Integer::parseInt).min();
+        int max = optionalMax.orElse(0);
+        int min = optionalMin.orElse(0);
+
+        System.out.println("Максимальная цифра в числе " + max + "," + " Минимальная цифра в числе " + min);
 
     }
 }
