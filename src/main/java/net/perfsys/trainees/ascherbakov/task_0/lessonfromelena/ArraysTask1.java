2115e9af729172cdc350225153c18a5bb2f205d6
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-03-07 15:44:08 +0200

    Create ArraysTask1.java

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonfromelena/ArraysTask1.java
@@ -0,0 +1,26 @@
+//Задача 1 «Половина массива»
+//Заполнить первую половину массива, состоящего из n элементов (n – четное) случайными числами в диапазоне [1;20]. Вторую половину массива получить как копию первой половины массива.
+//Например, элементы массива A (n=10): 3, 6, 10, 16, 11, 3, 6, 10, 16, 11
+
+
+package net.perfsys.trainees.ascherbakov.task_0.lessonfromelena;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArraysTask1 {
+    public static void main(String[] args) {
+        int n = 10;
+        int[] array = new int[n];
+
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (new Random()).ints(1, 20).iterator().nextInt();
+
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println();
+
+    }
+
+}
