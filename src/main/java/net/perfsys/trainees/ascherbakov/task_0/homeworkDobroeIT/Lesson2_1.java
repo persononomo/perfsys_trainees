7a34d4fd58b263c805d8e56ab792ec1684df62b7
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-03-11 12:58:05 +0200

    fix

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/homeworkDobroeIT/Lesson2_1.java
@@ -0,0 +1,19 @@
+//Написать метод, который получает на вход 3 числа и возвращает сумму лишь положительных из них. К примеру, на вход 1, 100, -2 = 101(-2 - отрицательное).
+
+package net.perfsys.trainees.ascherbakov.task_0.homeworkDobroeIT;
+
+public class Lesson2_1 {
+    public static void main(String[] args) {
+        System.out.println(getSumOfPositive(2, 4, -6));
+    }
+
+    public static int getSumOfPositive(int a, int b, int c) {
+        int result = 0;
+        if (a > 0 && b > 0 && c > 0) result = a + b + c;
+        else if (a > 0 && b < 0 && c > 0) result = a + c;
+        else if (a < 0 && b > 0 && c > 0) result = b + c;
+        else if (a > 0 && b > 0 && c < 0) result = a + b;
+        else System.out.println("Нельзя получить сумму, так как 2 числа отрицательны");
+        return result;
+    }
+}
