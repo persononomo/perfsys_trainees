98d6c62f930a7f5302b9d3ece5ca65d60f79eaaa
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-03-11 23:27:29 +0200

    fix

--- src/main/java/net/perfsys/trainees/ascherbakov/task_0/homeworkDobroeIT/Lesson2_1.java
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/homeworkDobroeIT/Lesson2_1.java
@@ -4,16 +4,19 @@ package net.perfsys.trainees.ascherbakov.task_0.homeworkDobroeIT;
 
 public class Lesson2_1 {
     public static void main(String[] args) {
-        System.out.println(getSumOfPositive(2, 4, -6));
+        System.out.println(getSumOfPositive(10, 2, 3));
     }
 
     public static int getSumOfPositive(int a, int b, int c) {
         int result = 0;
-        if (a > 0 && b > 0 && c > 0) result = a + b + c;
-        else if (a > 0 && b < 0 && c > 0) result = a + c;
-        else if (a < 0 && b > 0 && c > 0) result = b + c;
-        else if (a > 0 && b > 0 && c < 0) result = a + b;
-        else System.out.println("Нельзя получить сумму, так как 2 числа отрицательны");
+//        if (a > 0 && b > 0 && c > 0) result = a + b + c;
+////        else if (a > 0 && b < 0 && c > 0) result = a + c;
+////        else if (a < 0 && b > 0 && c > 0) result = b + c;
+////        else if (a > 0 && b > 0 && c < 0) result = a + b;
+////        else System.out.println("Нельзя получить сумму, так как 2 числа отрицательны");
+        if (a>0) result+=a;
+        if (b>0) result+=b;
+        if (c>0) result+=c;
         return result;
     }
 }
