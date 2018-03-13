0c37f86f19ae1e022184bf7e4d0deaf14d7b422d
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-03-13 15:36:05 +0200

    Create ArraysTask7.java

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/lessonfromelena/ArraysTask7.java
@@ -0,0 +1,34 @@
+//Задача 7 «Сумма цифр числа»
+//Написать функцию, которая определяет сумму цифр целого числа. Создать массив из n случайных целых чисел в диапазоне [10…1000].
+//Используя созданную функцию, вывести в список все числа, сумма цифр в которых минимальная.
+
+package net.perfsys.trainees.ascherbakov.task_0.lessonfromelena;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArraysTask7 {
+    public static void main(String[] args) {
+        int n = 10;
+        int[] array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (new Random()).ints(10, 1000).iterator().nextInt();
+        }
+        for (int num : array) {
+            System.out.print(num + "\t");
+
+        }
+
+
+    }
+
+    public static int sumOfNums(int a) {
+        String str = String.valueOf(a);
+        int[] numArr = Arrays.stream(str.split("")).mapToInt(Integer::parseInt).toArray();
+        int sum = Arrays.stream(numArr).sum();
+        return sum;
+    }
+
+}
+
