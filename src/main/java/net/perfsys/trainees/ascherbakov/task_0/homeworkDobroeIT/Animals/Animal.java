0f6142c55ede40b189452a9fec58ee9ba263686a
Fuzzywuzzy1991 <34865253+Fuzzywuzzy1991@users.noreply.github.com>
2018-03-18 18:26:01 +0200

    fix

---
+++ src/main/java/net/perfsys/trainees/ascherbakov/task_0/homeworkDobroeIT/Animals/Animal.java
@@ -0,0 +1,12 @@
+package net.perfsys.trainees.ascherbakov.task_0.homeworkDobroeIT.Animals;
+
+public class Animal {
+    int age;
+    String name;
+    static int sumAnimals = 0;
+
+    void voice() {
+    }
+
+}
+
